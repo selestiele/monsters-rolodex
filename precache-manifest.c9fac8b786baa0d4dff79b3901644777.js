fcb8789cc764cd641cf79c67be7b553c77beb035
Liz <admin@elizabethmbank.info>
2019-12-16 11:04:52 -0500

    Updates

---
+++ precache-manifest.c9fac8b786baa0d4dff79b3901644777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374c11b6e8b657f304f0efa50f7072d3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a70643ab8a92e586ea38",
+    "url": "/monsters-rolodex/static/css/main.b322237b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a70643ab8a92e586ea38",
+    "url": "/monsters-rolodex/static/js/main.021ae0f5.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
